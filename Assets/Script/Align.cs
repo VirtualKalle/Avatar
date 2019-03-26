8a16035ac2ddd11d20dec3a9c6b6b3ff6781ac6e
VirtualKalle <38258930+VirtualKalle@users.noreply.github.com>
2019-03-26 16:55:29 +0100

    First commit

---
+++ Assets/Script/Align.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Align : MonoBehaviour
+{
+
+    [SerializeField] Rigidbody go;
+    public Vector3 torque;
+    public Vector3 force;
+
+    [SerializeField] string torqueStr;
+    [SerializeField] Text textComponent1;
+    [SerializeField] Text textComponent2;
+    [SerializeField] float forceScaler;
+    [SerializeField] float torqueScaler;
+
+    Vector3 x;
+    float theta;
+    Vector3 w;
+    Quaternion q;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Force();
+        TorqueUp();
+        TorqueRight();
+        GravityTorque();
+    }
+
+    void Force()
+    {
+        force = (transform.position - go.transform.position) * forceScaler;
+        go.AddForceAtPosition(force,transform.position);
+    }
+
+    void GravityTorque()
+    {
+        Vector3 g = Vector3.Cross(go.transform.position - go.worldCenterOfMass, new Vector3(0, -1, 0) - go.worldCenterOfMass);
+        go.AddTorque(-((go.position - go.worldCenterOfMass).magnitude * go.mass * 9.82f * g));
+    }
+
+    void TorqueUp()
+    { 
+        x = Vector3.Cross(go.transform.up.normalized, transform.up.normalized);
+        theta = Mathf.Asin(x.magnitude);
+        //w = x.normalized * theta / Time.fixedDeltaTime;
+        w = x.normalized * theta;
+
+        Quaternion q = go.transform.rotation * go.inertiaTensorRotation;
+        torque = q * Vector3.Scale(go.inertiaTensor, (Quaternion.Inverse(q) * w)) * torqueScaler;
+
+        go.AddTorque(torque, ForceMode.Impulse);
+
+    }
+
+    void TorqueRight()
+    {
+        x = Vector3.Cross(go.transform.right.normalized, transform.right.normalized);
+        theta = Mathf.Asin(x.magnitude);
+        //w = x.normalized * theta / Time.fixedDeltaTime;
+        w = x.normalized * theta;
+
+        Quaternion q = go.transform.rotation * go.inertiaTensorRotation;
+        torque = q * Vector3.Scale(go.inertiaTensor, (Quaternion.Inverse(q) * w)) * torqueScaler;
+
+        go.AddTorque(torque, ForceMode.Impulse);
+
+    }
+
+    
+
+    private Vector3 RectifyAngleDifference(Vector3 angdiff)
+    {
+        if (angdiff.x > 180) angdiff.x -= 360;
+        if (angdiff.y > 180) angdiff.y -= 360;
+        if (angdiff.z > 180) angdiff.z -= 360;
+        return angdiff;
+    }
+
+}
---
+++ Assets/Script/AvatarManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarManager : MonoBehaviour {
+
+    [SerializeField] Transform handRightAvatar;
+    [SerializeField] Transform handLeftAvatar;
+    [SerializeField] Transform headAvatar;
+    [SerializeField] Transform bodyAvatar;
+
+    [SerializeField] Transform handRight;
+    [SerializeField] Transform handLeft;
+
+
+    [SerializeField] Transform head;
+
+    //[SerializeField] Transform body;
+    //Vector3 bodyRelativePosition;
+    //Quaternion bodyRelativeRotation;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void Move()
+    {
+        Debug.Log("MoVE" + OVRInput.Get(OVRInput.RawAxis2D.RThumbstick));
+        if (OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).magnitude > 0.1f)
+        {
+            Debug.Log("Input thumpstick");
+            transform.Translate(new Vector3(0, 0, OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).y) * Time.deltaTime, Space.Self);
+
+            transform.Rotate(0, OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).x * 10,0, Space.Self);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        FollowTransform();
+        Move();
+    }
+
+    void FollowTransform()
+    {
+        //headAvatar.localPosition = head.position;
+        //headAvatar.localRotation = head.localRotation;
+
+        //bodyAvatar.localPosition = headAvatar.localPosition + new Vector3(0,-1f,0);
+
+        //handRightAvatar.localPosition = handRight.position - head.position + headAvatar.localPosition;
+        handRightAvatar.localPosition = handRight.localPosition + headAvatar.localPosition;
+        handRightAvatar.localRotation = handRight.localRotation;
+
+        handLeftAvatar.localPosition = handLeft.position - head.position + headAvatar.localPosition;
+        handLeftAvatar.localRotation = handLeft.rotation;
+    }
+
+}
