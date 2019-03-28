e74c869d6b5714f9098a5247f1088bb01bdcea4e
VirtualKalle <38258930+VirtualKalle@users.noreply.github.com>
2019-03-28 09:08:01 +0100

    Instant rotation

--- Assets/Script/AvatarManager.cs
+++ Assets/Script/AvatarManager.cs
@@ -27,13 +27,14 @@ public class AvatarManager : MonoBehaviour {
 
     private void Move()
     {
-        Debug.Log("MoVE" + OVRInput.Get(OVRInput.RawAxis2D.RThumbstick));
+        //Debug.Log("MoVE" + OVRInput.Get(OVRInput.RawAxis2D.RThumbstick));
         if (OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).magnitude > 0.1f)
         {
             Debug.Log("Input thumpstick");
-            transform.Translate(new Vector3(0, 0, OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).y) * Time.deltaTime, Space.Self);
+            //transform.Translate(new Vector3(0, 0, OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).y) * Time.deltaTime, Space.Self);
+            transform.Translate(new Vector3(OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).x, 0, OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).y) * Time.deltaTime, Space.World);
 
-            transform.Rotate(0, OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).x * 10,0, Space.Self);
+            //transform.Rotate(0, OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).x * 2, 0, Space.Self);
         }
     }
 
@@ -41,6 +42,7 @@ public class AvatarManager : MonoBehaviour {
     void Update () {
         FollowTransform();
         Move();
+        Twist();
     }
 
     void FollowTransform()
@@ -56,6 +58,33 @@ public class AvatarManager : MonoBehaviour {
 
         handLeftAvatar.localPosition = handLeft.localPosition + headAvatar.localPosition;
         handLeftAvatar.localRotation = handLeft.localRotation;
+    }
+
+    void Twist()
+    {
+        //h1 = CrossPlatformInputManager.GetAxis("Horizontal"); // set as your inputs 
+        float h1 = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).x; // set as your inputs 
+        float v1 = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).y;
+
+        if (h1 == 0f && v1 == 0f)
+        { // this statement allows it to recenter once the inputs are at zero 
+            Vector3 curRot = transform.localEulerAngles; // the object you are rotating
+            Vector3 homeRot;
+            if (curRot.y > 180f)
+            { // this section determines the direction it returns home 
+                //Debug.Log(curRot.y);
+                homeRot = new Vector3(0f, 359.999f, 0f); //it doesnt return to perfect zero 
+            }
+            else
+            {                                                                      // otherwise it rotates wrong direction 
+                homeRot = Vector3.zero;
+            }
+            //transform.localEulerAngles = Vector3.Slerp(curRot, homeRot, Time.deltaTime * 2);
+        }
+        else
+        {
+            transform.localEulerAngles = new Vector3(0f, Mathf.Atan2(h1, v1) * 180 / Mathf.PI, 0f); // this does the actual rotaion according to inputs
+        }
     }
 
 }
