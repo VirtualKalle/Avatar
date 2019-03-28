72f909e2cc8c85f48577f16ff9e834e63060d785
VirtualKalle <38258930+VirtualKalle@users.noreply.github.com>
2019-03-28 09:03:27 +0100

    Added enemy system

---
+++ Assets/EnemyPhysics.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPhysics : MonoBehaviour {
+
+    [SerializeField] GameObject[] bodyParts;
+    EnemyHealth m_enemyHealth;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_enemyHealth = GetComponent<EnemyHealth>();
+        m_enemyHealth.deathEvent += Death;
+    }
+
+    private void OnDisable()
+    {
+        m_enemyHealth.deathEvent -= Death;
+    }
+
+    void Death()
+    {
+        Destroy(GetComponent<Rigidbody>());
+        foreach (GameObject go in bodyParts)
+        {
+            go.AddComponent<Rigidbody>();
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/StrikeDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrikeDamage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("hit");
+        EnemyHealth enemyHealth = collision.collider.GetComponentInParent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.TakeDamage(10, collision.contacts[0].point);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
