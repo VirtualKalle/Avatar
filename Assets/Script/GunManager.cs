9532e4b92ca88e84428b715e153f9521de88f832
VirtualKalle <38258930+VirtualKalle@users.noreply.github.com>
2019-04-03 15:35:04 +0200

    Gun update

---
+++ Assets/Script/GunManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+using System;
+
+public struct TransformRecording
+{
+    public Vector3 position;
+    public Quaternion rotation;
+    public bool shoot;
+
+    public TransformRecording(Vector3 position, Quaternion rotation, bool shoot) : this()
+    {
+        this.position = position;
+        this.rotation = rotation;
+        this.shoot = shoot;
+    }
+}
+
+public class GunManager : MonoBehaviour
+{
+
+    Item item;
+    public float shootCoolDownTimeLeft { get; private set; }
+    public WarpManager m_warpManager;
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject muzzle;
+
+    private bool isGrabbed;
+    private bool shootCoolDown;
+    private float shootCoolDownTime = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        item = GetComponent<Item>();
+    }
+
+    void OnTriggerShoot()
+    {
+        if (/*isGrabbed*/true)
+        {
+            //Debug.Log("OnTriggerShoot Start");
+            if (item.m_hand == Hand.Left && OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch) > 0.1f)
+            {
+                //Debug.Log("Shoot left");
+                TryShoot();
+            }
+            else if (item.m_hand == Hand.Right && OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTouch) > 0.1f)
+            {
+                //Debug.Log("Shoot right");
+                TryShoot();
+            }
+            //Debug.Log("OnTriggerShoot End");
+        }
+    }
+    
+    void TryShoot()
+    {
+        if (shootCoolDownTimeLeft <= 0)
+        {
+            if (GameManager.bulletTime)
+            {
+                m_warpManager.QueueAction();
+            }
+            else
+            {
+            Shoot(muzzle.transform.position, muzzle.transform.rotation);
+            }
+            shootCoolDownTimeLeft = shootCoolDownTime;
+        }
+
+    }
+
+    public void Shoot(Vector3 pos, Quaternion rot)
+    {
+        GameObject bulletClone = Instantiate(bullet, pos, rot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!item.isMoving && !item.holstered)
+        {
+        //CheckGrab();
+        OnTriggerShoot();
+        }
+
+        ShootCountDown();
+    }
+
+    private void ShootCountDown()
+    {
+        if (shootCoolDownTimeLeft > 0)
+        {
+            shootCoolDownTimeLeft -= Time.unscaledDeltaTime;
+        }
+    }
+       
+}
