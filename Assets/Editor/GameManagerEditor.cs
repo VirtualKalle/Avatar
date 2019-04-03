5e72316b6ceae2ab37e02f27c46db3f4bf28917d
VirtualKalle <38258930+VirtualKalle@users.noreply.github.com>
2019-04-03 15:45:12 +0200

    Editor folder

---
+++ Assets/Editor/GameManagerEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameManager))]
+public class GameManagerEditor : Editor
+{
+
+    bool debug;
+
+    public override void OnInspectorGUI()
+    {
+        debug = EditorGUILayout.Toggle("Debug Mode", debug);
+
+        if(debug)
+        {
+            GameManager.DebugMode = true;
+        }
+        else
+        {
+            GameManager.DebugMode = false;
+        }
+    }
+}
