f3e16d4aa813da81a7c7a35e8d0626d68f44d8a7
VirtualKalle <38258930+VirtualKalle@users.noreply.github.com>
2019-04-12 12:31:08 +0200

    Started scriptable object for scenemanagement

---
+++ Assets/Script/ScriptableObjects/SceneManagerSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "SceneManagerSO", menuName = "SceneManagerSO", order = 1)]
+public class SceneManagerSO : ScriptableObject {
+
+    [SerializeField] public string[] Scenes;
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadSceneAsync(scene, LoadSceneMode.Single);
+    }
+
+}
