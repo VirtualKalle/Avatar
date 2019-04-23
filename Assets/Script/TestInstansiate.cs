1c934fcbf270d01ece17f391b3595003d9cdc382
VirtualKalle <38258930+VirtualKalle@users.noreply.github.com>
2019-04-23 15:13:00 +0200

    Scenes update

---
+++ Assets/Script/TestInstansiate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInstansiate : MonoBehaviour {
+
+    [SerializeField] GameObject prefab;
+    Collider m_collider;
+	// Use this for initialization
+	void Start () {
+        m_collider = GetComponent<Collider>();
+
+    }
+	
+
+    void asdf()
+    {
+        m_collider.enabled = false;
+        GameObject leftGO = Instantiate(prefab,Vector3.zero, transform.rotation);
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            asdf();
+        }
+    }
+}
