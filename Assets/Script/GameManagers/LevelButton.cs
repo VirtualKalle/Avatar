9d8607251faf41b65ac9bf77214d865c4b1e2d88
VirtualKalle <38258930+VirtualKalle@users.noreply.github.com>
2019-05-20 11:20:49 +0200

    Added Start button

---
+++ Assets/Script/GameManagers/LevelButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelButton : MonoBehaviour {
+
+    LevelManager levelManager;
+
+    private void Awake()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    private void OnDestroy()
+    {
+        levelManager.StartLevel();
+    }
+}
